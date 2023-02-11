46cb72cf7ac43fc4d92fa4b9d9205ad7b99b3806
Juan René Hernández Sánchez <jhernandezs1509@alumno.ipn.mx>
2023-02-10 22:49:43 -0600

    Update Collition

--- Julia_ECA.jl
+++ Julia_ECA.jl
@@ -1,18 +1,23 @@
+import Pkg
+Pkg.add("Images")
+
 using Images
 
-const lines = 500
+const lines = 1200
 
 const ether = "11111000100110"
 const d1_c_f3_1 = "1111100010011011111100110"
 const c1_a_f1_1 = "111110000"
 const em_b_f1_1 = "111110111111011111010"
 const em_d_f3_1 = "11111000100110011000111111000100110"
-const em_c_f1_1 = "111110001011000"
+const em_c_f1_1 = "111110001011000111010"
 const em_h_f2_1 = "111110001001111111000"
 const em_e_f1_1 = "111110111010111000110"
 const a4_f3_1 = "11111000100110100110011001100110"
-const a4_f2_1 = "11111000111000100110011001100110"
-const a4_f1_1 = "111110110110110"
+const a3_f2_1 = "1111100011101110111000100110"
+const a2_f1_1 = "1111101110"
+
+const t1 = "0"
 
 const g_gun_collitions = d1_c_f3_1 * ether * c1_a_f1_1 * ether * em_b_f1_1
 
@@ -20,16 +25,18 @@ const g_gun_collitions = d1_c_f3_1 * ether * c1_a_f1_1 * ether * em_b_f1_1
 # -A^4(f1_1)-e-[E-](B,f1_1)-e-[E-](D,f3_1)-2e-
 # -[E-](C,f1_1)-2e-[E-](H,f2_1)-2e-
 # -[E-](E,f1_1)-2e-[E-](C,f1_1)-e*.
-const tag = a4_f3_1 * repeat(ether, 13) * a4_f2_1 * repeat(ether, 3) *
-            a4_f1_1 * ether * em_b_f1_1 * ether * em_d_f3_1 * repeat(ether, 2) *
+const tag = a4_f3_1 * repeat(ether, 13) * a3_f2_1 * repeat(ether, 13) *
+            a2_f1_1 * ether * em_b_f1_1 * ether * em_d_f3_1 * repeat(ether, 2) *
             em_c_f1_1 * repeat(ether, 2) * em_h_f2_1 * repeat(ether, 2) *
             em_e_f1_1 * repeat(ether, 2) * em_c_f1_1
 
-const start = repeat(ether, 20) * tag * repeat(ether, 20)
+const start = repeat(ether, 1) * tag * repeat(ether, 1)
+const lengthSImg = length(start)
 
 const rules = [110]
+const gray = RGB{N0f8}(0.350, 0.350, 0.350)
 const white = RGB{N0f8}(0.999, 0.999, 0.999)
-const black = RGB{N0f8}(0.001, 0.001, 0.001)
+const black = RGB{N0f8}(0.000, 0.000, 0.000)
 
 rule2poss(rule) = [rule & (1 << (i - 1)) != 0 for i in 1:8]
 
@@ -49,7 +56,7 @@ end
 const startset = cells2bools(start)
 
 function printRule(rul)
-    img = zeros(RGB{N0f8}, lines, length(start))
+    img = zeros(RGB{N0f8}, lines, lengthSImg)
     println("\nUsing Rule $rul:")
     bset = vcat(startset[end], startset, startset[1]) # wrap ends
     rp = rule2poss(rul)
@@ -59,6 +66,13 @@ function printRule(rul)
         bset = transform(bset, rp)
     end
     return img
+end
+
+function teselationColor(image)
+    booleanMatrix = zeros(Int8, lines, lengthSImg)
+    for x in 1:lines, y in 1:lengthSImg
+
+    end
 end
 
 img = printRule(rules[1])
